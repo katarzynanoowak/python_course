11e814a27794eb507dc3f4ce400e05cc418aa436
katarzyna <katarzyna.noowak@gmail.com>
2020-03-30 10:54:18 +0200

    test commit

--- add_group_test.py
+++ add_group_test.py
@@ -11,15 +11,15 @@ class TestAddGroup(unittest.TestCase):
     def open_home_page(self, wd):
             wd.get("http://localhost/addressbook/addressbook/group.php")
 
-    def test_add_group(self):
-        wd = self.wd
-        self.open_home_page(wd)
-        # log in
+    def login(self, wd):
         wd.find_element_by_name("user").send_keys("admin")
         wd.find_element_by_name("pass").send_keys("secret")
         wd.find_element_by_xpath("//input[@value='LOGIN']").click()
-        # open group page
+
+    def open_groups_page(self, wd):
         wd.find_element_by_link_text("GROUPS").click()
+
+    def create_group(self, wd):
         # init group creation
         wd.find_element_by_name("new").click()
         # fill group form
@@ -28,12 +28,21 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element_by_name("group_footer").send_keys("test footer")
         # submit group creation
         wd.find_element_by_name("submit").click()
-        # return to groups page
+
+    def return_to_groups_page(self, wd):
         wd.find_element_by_link_text("group page").click()
-        # logout
-        wd.find_element_by_link_text("LOGOUT").click()
 
+    def logout(self, wd):
+            wd.find_element_by_link_text("LOGOUT").click()
 
+    def test_add_group(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd)
+        self.open_groups_page(wd)
+        self.create_group(wd)
+        self.return_to_groups_page(wd)
+        self.logout(wd)
 
     def tearDown(self):
         self.wd.quit()
