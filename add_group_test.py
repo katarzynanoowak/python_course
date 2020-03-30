30e465224912bcd788dab3e6a6de851c424e15a8
katarzyna <katarzyna.noowak@gmail.com>
2020-03-30 11:44:33 +0200

    Added improved methods with parameters

--- add_group_test.py
+++ add_group_test.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
 import unittest
+from group import Group
 
 
 class TestAddGroup(unittest.TestCase):
@@ -9,7 +10,7 @@ class TestAddGroup(unittest.TestCase):
         self.wd.implicitly_wait(30)
 
     def open_home_page(self, wd):
-            wd.get("http://localhost/addressbook/addressbook/group.php")
+        wd.get("http://localhost/addressbook/addressbook/group.php")
 
     def login(self, wd, username, password):
         wd.find_element_by_name("user").send_keys(username)
@@ -19,13 +20,13 @@ class TestAddGroup(unittest.TestCase):
     def open_groups_page(self, wd):
         wd.find_element_by_link_text("GROUPS").click()
 
-    def create_group(self, wd, name, header, footer):
+    def create_group(self, wd, group):
         # init group creation
         wd.find_element_by_name("new").click()
         # fill group form
-        wd.find_element_by_name("group_name").send_keys(name)
-        wd.find_element_by_name("group_header").send_keys(header)
-        wd.find_element_by_name("group_footer").send_keys(footer)
+        wd.find_element_by_name("group_name").send_keys(group.name)
+        wd.find_element_by_name("group_header").send_keys(group.header)
+        wd.find_element_by_name("group_footer").send_keys(group.footer)
         # submit group creation
         wd.find_element_by_name("submit").click()
 
@@ -33,14 +34,14 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element_by_link_text("group page").click()
 
     def logout(self, wd):
-            wd.find_element_by_link_text("LOGOUT").click()
+        wd.find_element_by_link_text("LOGOUT").click()
 
     def test_add_group(self):
         wd = self.wd
         self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
         self.open_groups_page(wd)
-        self.create_group(wd, name="test name", header="test header", footer="test footer")
+        self.create_group(wd, Group(name="test name", header="test header", footer="test footer"))
         self.return_to_groups_page(wd)
         self.logout(wd)
 
@@ -49,7 +50,7 @@ class TestAddGroup(unittest.TestCase):
         self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
         self.open_groups_page(wd)
-        self.create_group(wd, name="", header="", footer="")
+        self.create_group(wd, Group(name="", header="", footer=""))
         self.return_to_groups_page(wd)
         self.logout(wd)
 
