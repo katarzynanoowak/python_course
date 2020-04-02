95f5ae4a8cec08ba26923e7d7d3533701c722aa2
katarzyna <katarzyna.noowak@gmail.com>
2020-04-02 16:20:44 +0200

    Extracted class SessionHelper.

--- fixture/application.py
+++ fixture/application.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from fixture.session import SessionHelper
 
 
 class Application:
@@ -6,17 +7,12 @@ class Application:
     def __init__(self):
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
+        self.session = SessionHelper(self)
 
     def open_home_page(self):
         wd = self.wd
         wd.get("http://localhost/addressbook/addressbook/group.php")
 
-    def login(self, username, password):
-        wd = self.wd
-        self.open_home_page()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//input[@value='LOGIN']").click()
 
     def open_groups_page(self):
         wd = self.wd
@@ -39,9 +35,6 @@ class Application:
         wd = self.wd
         wd.find_element_by_link_text("group page").click()
 
-    def logout(self):
-        wd = self.wd
-        wd.find_element_by_link_text("LOGOUT").click()
 
     def destroy(self):
         self.wd.quit()
--- test/add_group_test.py
+++ test/add_group_test.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import pytest
-from group import Group
-from application import Application
+from model.group import Group
+from fixture.application import Application
 
 
 @pytest.fixture()
@@ -12,12 +12,12 @@ def app(request):
 
 
 def test_add_group(app):
-        app.login(username="admin", password="secret")
+        app.session.login(username="admin", password="secret")
         app.create_group(Group(name="test name", header="test header", footer="test footer"))
-        app.logout()
+        app.session.logout()
 
 
 def test_add_empty_group(app):
-        app.login(username="admin", password="secret")
+        app.session.login(username="admin", password="secret")
         app.create_group(Group(name="", header="", footer=""))
-        app.logout()
+        app.session.logout()
