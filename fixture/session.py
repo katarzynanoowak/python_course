abe19477919e6cec95ebf73d44442b18395594fb
katarzyna <katarzyna.noowak@gmail.com>
2020-04-09 16:45:21 +0200

    Added fixture validation and current session state checking. Adjusted SessionHelper class accordingly. Assignment task 8.

--- fixture/session.py
+++ fixture/session.py
@@ -6,7 +6,7 @@ class SessionHelper:
     def login(self, username, password):
         wd = self.app.wd
         # added delete_all_cookies() to ensure that tests can run altogether in the same session
-        wd.delete_all_cookies()
+        # wd.delete_all_cookies()
         self.app.open_home_page()
         wd.find_element_by_name("user").send_keys(username)
         wd.find_element_by_name("pass").send_keys(password)
@@ -15,3 +15,25 @@ class SessionHelper:
     def logout(self):
         wd = self.app.wd
         wd.find_element_by_link_text("LOGOUT").click()
+
+    def ensure_logout(self):
+        # wd = self.app.wd
+        if self.is_logged_in():
+            self.logout()
+
+    def is_logged_in(self):
+        wd = self.app.wd
+        return len(wd.find_elements_by_link_text("LOGOUT")) > 0
+
+    def is_logged_in_as(self, username):
+        wd = self.app.wd
+        return wd.find_element_by_xpath("//*[@id='top']/form/b").text == "("+username+")"
+
+    def ensure_login(self, username, password):
+        # wd = self.app.wd
+        if self.is_logged_in():
+            if self.is_logged_in_as(username):
+                return
+            else:
+                self.logout()
+        self.login(username, password)
--- test/add_group_test.py
+++ test/add_group_test.py
@@ -3,12 +3,10 @@ from model.group import Group
 
 
 def test_add_group(app):
-    app.session.login(username="admin", password="secret")
     app.group.create(Group(name="test name", header="test header", footer="test footer"))
+    # logout to ensure that fixture validation works as intended
     app.session.logout()
 
 
 def test_add_empty_group(app):
-    app.session.login(username="admin", password="secret")
     app.group.create(Group(name="", header="", footer=""))
-    app.session.logout()
